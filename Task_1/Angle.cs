13f4cca991e0b7c6550637a3dea025c8e7bd49ee
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-20 22:42:07 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Angle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Angle
+    {
+        private int gradus;
+        private int min;
+        private int sec;
+
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("MISTAKE!");
+                else gradus = value;
+            }
+        }
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("MISTAKE!");
+                else min = value;
+            }
+        }
+        public int Sec
+        {
+            get { return sec; }
+            set
+            {
+                if (value < 0)
+                    Console.WriteLine("MISTAKE!");
+                else sec = value;
+            }
+        }
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public Angle()
+        {
+            this.Gradus = 0;
+            this.Min = 0;
+            this.Sec = 0;
+        }
+        public double ToRadians()
+        {
+            return ((gradus +(min + sec/60.0)/60.0)*Math.PI)/180.0;
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle a = new Angle ();
+            Console.WriteLine("Введите значение градуса °");
+            a.Gradus = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение угловых минут");
+            a.Min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение угловых секунд");
+            a.Sec = Convert.ToInt32(Console.ReadLine());
+            int x = a.Gradus * 2;
+            Console.WriteLine("{0}° {1}' {2}\" = {3} рад", a.Gradus, a.Min, a.Sec, a.ToRadians());
+        }
+    }
+}
